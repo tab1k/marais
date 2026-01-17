ff655ad59b6d3c9328bf295a81a641be127fe21c
tab1k <tab1k@example.com>
2026-01-17 09:59:20 +0500

    refactor: Update product detail URL parameter type from `slug` to `str`.

--- src/catalog/urls.py
+++ src/catalog/urls.py
@@ -5,7 +5,7 @@ app_name = 'catalog'
 
 urlpatterns = [
 	path('', CatalogView.as_view(), name='home'),
-	path('detail/<slug:slug>/', ProductDetailView.as_view(), name='detail'),
+	path('detail/<str:slug>/', ProductDetailView.as_view(), name='detail'),
     path('search/suggestions/', SearchSuggestionsView.as_view(), name='search_suggestions'),
 
 	path('profile/', ProfileView.as_view(), name='profile'),
