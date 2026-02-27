9025880c7d180465d402abb5f0411577e0436738
tab1k <tab1k@example.com>
2026-02-27 21:47:43 +0500

    add: implement privacy policy, public offer, and payment info pages; update footer and header links

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.GeneralPageView.as_view(), name='general'),
@@ -7,4 +8,7 @@ urlpatterns = [
     path('brand/<slug:slug>/', views.BrandDetailView.as_view(), name='brand_detail'),
     path('project/', views.ProjectPageView.as_view(), name='project'),
     path('api/subscribe/', views.NewsletterSubscribeView.as_view(), name='subscribe'),
+    path('privacy-policy/', TemplateView.as_view(template_name='main/privacy_policy.html'), name='privacy_policy'),
+    path('public-offer/', TemplateView.as_view(template_name='main/public_offer.html'), name='public_offer'),
+    path('payment-info/', TemplateView.as_view(template_name='main/payment_info.html'), name='payment_info'),
 ]
